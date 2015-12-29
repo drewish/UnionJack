7fb87248ba6fb49a18b6a169f4f6c404bd322e1c
andrew morton <drewish@katherinehouse.com>
2015-12-28 18:10:13 -0700

    Add missing header changes
    Fixes #1.

--- include/UnionJack.h
+++ include/UnionJack.h
@@ -16,35 +16,43 @@ using namespace std;
 
 class UnionJack {
 public:
-    UnionJack( uint digits );
+    UnionJack( uint digits = 10, float slant = 0.2f );
 
-    // Chain-able configuration methods
+    // Set the xy position.
     UnionJack& position( const vec2 &p );
+    // Set the xy position to be below another display.
     UnionJack& below( const UnionJack &other );
+    // Set the xy position to be right of another display.
     UnionJack& rightOf( const UnionJack &other );
+    // Scale display. The default character size is 16 x 24.
     UnionJack& scale( const float &s );
+    // Set the on and off colors.
     UnionJack& colors( const ColorA &on, const ColorA &off );
+    // Set the text to display. Strings longer than the display size will be
+    // truncated. Strings shorter than the display size will be padded with
+    // spaces.
     UnionJack& display( string s );
 
-    void setup();
     void draw() const;
 
     const vec2 position() const { return mPosition; }
     float height() const;
     float width() const;
 
-    // Returns the pattern for a printable ASCII character in our font.
+    // Returns the pattern for a printable ASCII character (33-127) in our font.
     // Out of range values return blank.
     uint16_t valueOf( const char );
 
 protected:
+    void setup();
+
     uint            mDigits; // Number of characters in display
     ci::vec2        mPosition;
     float           mScale;
     float           mSlant; // Positive leans right, negative leans left
-    ci::vec4        mColors[2]; // 0 is off 1 is on
+    ci::vec4        mOnColor;
+    ci::vec4        mOffColor;
     gl::BatchRef    mBatch;
-
     gl::VboRef      mInstancePositionVbo;
     gl::VboRef      mInstanceValueVbo;
 };
